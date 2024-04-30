a209f0e6a8deb8d8c7ec92bf2aea2dc8ecf77041
QuiqueRobles <e.robles3@usp.ceu.es>
2024-04-30 12:01:20 +0200

    Payment form well-displayed, now has to become functional

--- website/static/checkout.js
+++ website/static/checkout.js
@@ -43,7 +43,7 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url: "http://localhost:4242/checkout.html",
+      return_url: "http://localhost:5000/home.html",
     },
   });
 
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Blueprint, render_template, request, flash, jsonify, url_for,Flask, current_app, redirect
 from flask_login import login_required, current_user
 from .models import User,Admin,Event
@@ -20,37 +21,8 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if request.method == 'POST': 
-        name=request.form.get('name')
-        date_str=request.form.get('date')
-        date = datetime.strptime(date_str, '%Y-%m-%d').date()
-        max_guest_num=int(request.form.get('max_guest_num'))
-        member_price=float(request.form.get('member_price'))
-        member_child_price=float(request.form.get('member_child_price'))
-        child_price=float(request.form.get('child_price'))
-        guest_price=float(request.form.get('guest_price'))
-        description=request.form.get('description')
-        event_image = request.files['img_url']
-        if event_image and allowed_file(event_image.filename):
-            # Asegurar el nombre del archivo
-            filename = secure_filename(event_image.filename)
-            # Guardar la imagen en la carpeta "static"
-            filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
-            print(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-            event_image.save(filepath)
-            print(url_for('static',filename=f'images/{filename}'))
-            # Obtener la URL relativa de la imagen
-            img_url = url_for('static',filename=f'images/{filename}')
-        else: 
-             img_url=url_for('static',filename=f'images/gremaLogo.png')
-            
-        #AQUÍ HAY QUE CONTROLAR ALGUNOS ERRORES AL RELLENAR EL FORM
-        new_event = Event(name=name, date=date, max_guest_num=max_guest_num, member_price=member_price, member_child_price=member_child_price, guest_price=guest_price, child_price=child_price, img_url=img_url, description=description, admin_id=current_user.id)
-        db.session.add(new_event) #adding the note to the database 
-        db.session.commit()
-        flash('Event added correctly!', category='success')
-
+        create_event()
     todos_los_eventos = Event.query.all()
-    # Filtra los eventos que aún no han pasado
     eventos_activos = [evento for evento in todos_los_eventos if evento.date >= datetime.now()]
     for event in eventos_activos:
         print(event.name)
@@ -72,25 +44,7 @@ def delete_event(event_id):
 
 @views.route('/event-attendance/<int:event_id>', methods=['GET', 'POST'])
 @login_required
-def event_attendance(event_id):
-
-    if request.method == 'POST':
-
-        event_id = request.form.get('event_id')
-        try:
-        #data = json.loads(request.data)
-        # Create a PaymentIntent with the order amount and currency
-            intent = stripe.PaymentIntent.create(
-                amount=50,
-                currency='eur'
-            )
-            return jsonify({
-            'clientSecret': intent['client_secret']
-        })
-        except Exception as e:
-                return jsonify(error=str(e)), 403
-        return redirect(url_for('event_details', event_id=event_id))
-    
+def event_attendance(event_id):    
     event = Event.query.get_or_404(event_id)
     return render_template('event.html', event=event, user=current_user)
 
@@ -100,14 +54,17 @@ def profile():
     return render_template('profile.html', user=current_user)
 
 @views.route('/create-payment-intent', methods=['POST'])
+@login_required
 def create_payment():
-    print("Realizando payment intent")
     try:
-        #data = json.loads(request.data)
-        # Create a PaymentIntent with the order amount and currency
+        data = json.loads(request.data)
         intent = stripe.PaymentIntent.create(
-            amount=0.50,
-            currency='eur'
+            #amount=calculate_order_amount(data['items']),
+            amount=500,
+            currency='eur',
+            automatic_payment_methods={
+                'enabled': True,
+            },
         )
         return jsonify({
             'clientSecret': intent['client_secret']
@@ -120,3 +77,41 @@ def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
 
 
+
+
+def create_event():
+    name=request.form.get('name')
+    date_str=request.form.get('date')
+    date = datetime.strptime(date_str, '%Y-%m-%d').date()
+    max_guest_num=int(request.form.get('max_guest_num'))
+    member_price=float(request.form.get('member_price'))
+    member_child_price=float(request.form.get('member_child_price'))
+    child_price=float(request.form.get('child_price'))
+    guest_price=float(request.form.get('guest_price'))
+    description=request.form.get('description')
+    event_image = request.files['img_url']
+    if event_image and allowed_file(event_image.filename):
+        # Asegurar el nombre del archivo
+        filename = secure_filename(event_image.filename)
+        # Guardar la imagen en la carpeta "static"
+        filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        print(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+        event_image.save(filepath)
+        print(url_for('static',filename=f'images/{filename}'))
+        # Obtener la URL relativa de la imagen
+        img_url = url_for('static',filename=f'images/{filename}')
+    else: 
+        img_url=url_for('static',filename=f'images/gremaLogo.png')
+        
+    #AQUÍ HAY QUE CONTROLAR ALGUNOS ERRORES AL RELLENAR EL FORM
+    new_event = Event(name=name, date=date, max_guest_num=max_guest_num, member_price=member_price, member_child_price=member_child_price, guest_price=guest_price, child_price=child_price, img_url=img_url, description=description, admin_id=current_user.id)
+    db.session.add(new_event) #adding the note to the database 
+    db.session.commit()
+    flash('Event added correctly!', category='success')
+    return 
+
+def calculate_order_amount(items):
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    return 1000
